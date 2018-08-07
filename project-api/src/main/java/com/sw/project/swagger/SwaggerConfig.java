08ff3f77e1fea180eac0e48ecb18c8d385228576
phantasmicmeans <dlaak0630@naver.com>
2018-08-08 00:25:24 +0900

    swagger

---
+++ project-api/src/main/java/com/sw/project/swagger/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.sw.project.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.sw.project.controller"))
+                .paths(PathSelectors.any()) //위 패키지에 swagger를 적용한다.
+                .build();
+
+    }
+}
